9901df3e19f786a4bce19041b02276d6ab39deee
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2021-08-22 08:39:05 +1200

    Vulkan Cleanup

--- Engine/src/Renderer/Vulkan/VulkanRenderer.c
+++ Engine/src/Renderer/Vulkan/VulkanRenderer.c
@@ -19,6 +19,12 @@ b8 VulkanRenderer_Create(Renderer* outRenderer, Surface* surface, String name) {
 
     data->Renderer = outRenderer;
 
+    *outRenderer = (Renderer){
+        ._Destroy = VulkanRenderer_Destroy,
+        ._Surface = surface,
+        ._PrivateData = data,
+    };
+
 #if defined(THALLIUM_PLATFORM_WINDOWS)
     data->VulkanDllHandle = LoadLibraryA("vulkan-1.dll");
     if (data->VulkanDllHandle == nil) goto Error;
@@ -27,9 +33,9 @@ b8 VulkanRenderer_Create(Renderer* outRenderer, Surface* surface, String name) {
     if (data->vkGetInstanceProcAddr == nil) goto Error;
 #endif
 
-#define FUNCTION(name) data->name = cast(PFN_ ## name) data->vkGetInstanceProcAddr(nil, #name); if (data->name == nil) goto Error;
-    FUNCTIONS
-#undef FUNCTION
+#define VULKAN_FUNCTION(name) data->name = cast(PFN_ ## name) data->vkGetInstanceProcAddr(nil, #name); if (data->name == nil) goto Error;
+    VULKAN_FUNCTIONS
+#undef VULKAN_FUNCTION
 
     const u32 RequiredAPIVersion = VK_API_VERSION_1_2;
 
@@ -54,19 +60,13 @@ b8 VulkanRenderer_Create(Renderer* outRenderer, Surface* surface, String name) {
         .ppEnabledExtensionNames = nil,
     }, data->Allocator, &data->Instance) != VK_SUCCESS || data->Instance == VK_NULL_HANDLE) goto Error;
 
-#define INSTANCE_FUNCTION(name) data->name = cast(PFN_ ## name) data->vkGetInstanceProcAddr(data->Instance, #name); if (data->name == nil) goto Error;
-    INSTANCE_FUNCTIONS
-#undef INSTANCE_FUNCTION
-
-    *outRenderer = (Renderer){
-        ._Destroy = VulkanRenderer_Destroy,
-        ._Surface = surface,
-        ._PrivateData = data,
-    };
+#define VULKAN_INSTANCE_FUNCTION(name) data->name = cast(PFN_ ## name) data->vkGetInstanceProcAddr(data->Instance, #name); if (data->name == nil) goto Error;
+    VULKAN_INSTANCE_FUNCTIONS
+#undef VULKAN_INSTANCE_FUNCTION
 
     return TRUE;
 Error:
-    Deallocate(data);
+    VulkanRenderer_Destroy(outRenderer);
     return FALSE;
 }
 
@@ -77,7 +77,9 @@ void VulkanRenderer_Destroy(Renderer* renderer) {
     
     VulkanRenderer* data = renderer->_PrivateData;
 
-    data->vkDestroyInstance(data->Instance, data->Allocator);
+    if (data->Instance) {
+        data->vkDestroyInstance(data->Instance, data->Allocator);
+    }
 
 #if defined(THALLIUM_PLATFORM_WINDOWS)
     FreeLibrary(data->VulkanDllHandle);
--- Engine/src/Renderer/Vulkan/VulkanRenderer.h
+++ Engine/src/Renderer/Vulkan/VulkanRenderer.h
@@ -4,14 +4,14 @@
 
 #include <vulkan/vulkan.h>
 
-#define FUNCTIONS \
-    FUNCTION(vkCreateInstance) \
-    FUNCTION(vkEnumerateInstanceVersion) \
-    FUNCTION(vkEnumerateInstanceLayerProperties) \
-    FUNCTION(vkEnumerateInstanceExtensionProperties)
+#define VULKAN_FUNCTIONS \
+    VULKAN_FUNCTION(vkCreateInstance) \
+    VULKAN_FUNCTION(vkEnumerateInstanceVersion) \
+    VULKAN_FUNCTION(vkEnumerateInstanceLayerProperties) \
+    VULKAN_FUNCTION(vkEnumerateInstanceExtensionProperties)
 
-#define INSTANCE_FUNCTIONS \
-    INSTANCE_FUNCTION(vkDestroyInstance)
+#define VULKAN_INSTANCE_FUNCTIONS \
+    VULKAN_INSTANCE_FUNCTION(vkDestroyInstance)
 
 typedef struct VulkanRenderer {
     Renderer* Renderer;
@@ -21,13 +21,13 @@ typedef struct VulkanRenderer {
 
     PFN_vkGetInstanceProcAddr vkGetInstanceProcAddr;
     
-#define FUNCTION(name) PFN_ ## name name;
-    FUNCTIONS
-#undef FUNCTION
+#define VULKAN_FUNCTION(name) PFN_ ## name name;
+    VULKAN_FUNCTIONS
+#undef VULKAN_FUNCTION
 
-#define INSTANCE_FUNCTION(name) PFN_ ## name name;
-    INSTANCE_FUNCTIONS
-#undef INSTANCE_FUNCTION
+#define VULKAN_INSTANCE_FUNCTION(name) PFN_ ## name name;
+    VULKAN_INSTANCE_FUNCTIONS
+#undef VULKAN_INSTANCE_FUNCTION
 
 #if defined(THALLIUM_PLATFORM_WINDOWS)
     void* VulkanDllHandle;
