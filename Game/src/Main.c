c2dfc3de206aaffe3993cfd68717254a82a648e8
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2021-08-23 10:16:13 +1200

    Added Both Renderers in Seperate Windows

--- Game/src/Main.c
+++ Game/src/Main.c
@@ -6,8 +6,10 @@
 typedef struct GameData {
     String Name;
     b8 Running;
-    Surface Surface;
-    Renderer Renderer;
+    Surface SurfaceVulkan;
+    Renderer RendererVulkan;
+    Surface SurfaceOpenGL;
+    Renderer RendererOpenGL;
 } GameData;
 
 static void OnCloseCallback(Surface* surface) {
@@ -24,41 +26,64 @@ static void OnKeyCallback(Surface* surface, KeyCode key, b8 pressed) {
 
 static void OnResizeCallback(Surface* surface, u32 width, u32 height) {
     GameData* data = cast(GameData*) surface->UserData;
-    Renderer_OnSurfaceResize(&data->Renderer, width, height);
-    LogTrace(String_FromLiteral("Resize: %u, %u"), width, height);
+    Renderer* renderer = surface == &data->SurfaceVulkan ? &data->RendererVulkan : &data->RendererOpenGL;
+    Renderer_OnSurfaceResize(renderer, width, height);
 }
 
 int main(int argc, char** argv) {
     GameData data = {};
     data.Name = String_FromLiteral("Test Game");
 
-    if (!Surface_Create(&data.Surface, data.Name, 640, 480)) {
+    if (!Surface_Create(&data.SurfaceVulkan, String_FromLiteral("Vulkan Renderer"), 640, 480)) {
+        return -1;
+    }
+
+    if (!Surface_Create(&data.SurfaceOpenGL, String_FromLiteral("OpenGL Renderer"), 640, 480)) {
         return -1;
     }
 
     data.Running = TRUE;
 
-    data.Surface.UserData = &data;
-    data.Surface.OnCloseCallback = OnCloseCallback;
-    data.Surface.OnKeyCallback = OnKeyCallback;
-    data.Surface.OnResizeCallback = OnResizeCallback;
+    data.SurfaceOpenGL.UserData = &data;
+    data.SurfaceOpenGL.OnCloseCallback = OnCloseCallback;
+    data.SurfaceOpenGL.OnKeyCallback = OnKeyCallback;
+    data.SurfaceOpenGL.OnResizeCallback = OnResizeCallback;
 
-    if (!Renderer_Create(&data.Renderer, RendererAPI_OpenGL, &data.Surface, data.Name)) {
+    data.SurfaceVulkan.UserData = &data;
+    data.SurfaceVulkan.OnCloseCallback = OnCloseCallback;
+    data.SurfaceVulkan.OnKeyCallback = OnKeyCallback;
+    data.SurfaceVulkan.OnResizeCallback = OnResizeCallback;
+
+    if (!Renderer_Create(&data.RendererVulkan, RendererAPI_Vulkan, &data.SurfaceVulkan, data.Name)) {
+        return -1;
+    }
+
+    if (!Renderer_Create(&data.RendererOpenGL, RendererAPI_OpenGL, &data.SurfaceOpenGL, data.Name)) {
         return -1;
     }
 
     while (data.Running) {
-        if (Renderer_BeginFrame(&data.Renderer, 0.0f)) {
-            if (!Renderer_EndFrame(&data.Renderer, 0.0f)) {
+        if (Renderer_BeginFrame(&data.RendererVulkan, 0.0f)) {
+            if (!Renderer_EndFrame(&data.RendererVulkan, 0.0f)) {
                 LogFatal(String_FromLiteral("Failed to end frame. Shutting Down..."));
                 return -1;
             }
         }
 
-        Surface_Update(&data.Surface);
+        if (Renderer_BeginFrame(&data.RendererOpenGL, 0.0f)) {
+            if (!Renderer_EndFrame(&data.RendererOpenGL, 0.0f)) {
+                LogFatal(String_FromLiteral("Failed to end frame. Shutting Down..."));
+                return -1;
+            }
+        }
+
+        Surface_Update(&data.SurfaceVulkan);
+        Surface_Update(&data.SurfaceOpenGL);
     }
 
-    Renderer_Destroy(&data.Renderer);
-    Surface_Destroy(&data.Surface);
+    Renderer_Destroy(&data.RendererVulkan);
+    Renderer_Destroy(&data.RendererOpenGL);
+    Surface_Destroy(&data.SurfaceVulkan);
+    Surface_Destroy(&data.SurfaceOpenGL);
     return 0;
 }
